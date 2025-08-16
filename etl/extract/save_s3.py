c6d189ea774efc046b493410f0be6da6f193914d
dxmension <h4nzarick.nurali@gmail.com>
2025-08-16 16:45:55 +0500

    add: logic for uploading raw json into s3

--- etl/extract/save_s3.py
+++ etl/extract/save_s3.py
@@ -1,1 +1,32 @@
+import os
+import json
 import boto3
+from typing import Any
+from datetime import datetime
+from dotenv import load_dotenv
+
+load_dotenv()
+
+AWS_SERVER_PUBLIC_KEY = os.environ.get("AWS_SERVER_PUBLIC_KEY", "")
+AWS_SERVER_SECRET_KEY = os.environ.get("AWS_SERVER_SECRET_KEY", "")
+
+class S3Service:
+    def __init__(self, bucket_name: str, region_name: str = "eu-north-1"):
+        self.bucket_name = bucket_name
+        self.session = boto3.Session(
+            aws_access_key_id=AWS_SERVER_PUBLIC_KEY,
+            aws_secret_access_key=AWS_SERVER_SECRET_KEY,
+            region_name=region_name
+        )
+        self.s3_client = self.session.resource("s3")
+    
+    def upload_json(self, data: Any, prefix: str, filename: str) -> str:
+        key = f"{prefix}/{filename}.json"
+        obj = self.s3_client.Object(self.bucket_name, key)
+
+        obj.put(
+            Body=json.dumps(data, ensure_ascii=False).encode("UTF-8"),
+            ContentType="application/json"
+        )
+
+        return key
